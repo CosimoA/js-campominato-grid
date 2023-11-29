41b9d03b14b8ed168b4f624af342d08905c31265
CosimoA <cosimo.archip@gmail.com>
2023-11-29 13:37:21 +0100

    CONFIG: js flux

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. 
 */
+
+// SELEZIONO ELEMENTI UTILI IN HTML & VARIABILI PREIMPOSTATE
+
+// FUNZIONE CREA ELEMENTO
+
+// AL CLICK SUL BOTTONE INSERISCO ELEMENTO SQUARE ALLINTERNO DEL CONTAINER
+    // CREO CICLO FOR
+        // CREO ELEMENTO DIV CON LA FUNZIONE E ASSEGNO LA CLASSE SQUARE
+        // INSERICO DENTRO AL DIV IL NUMERO DEL CICLO
+
+    // AL CLICK SULL'ELEMENTO SQUARE
+        // AGGIUNGO ALL'ELEMENTO CLICCATO LA CLASSE
+        // OUTPUT IN CONSOLE DEL NUMERO CLICCATO
+
+
+
