d3f3c8302737eb485c4956b40a974c20664e4888
CosimoA <cosimo.archip@gmail.com>
2023-11-29 14:13:27 +0100

    FEAT: event click create square & add class

--- js/script.js
+++ js/script.js
@@ -8,20 +8,42 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro e
 // SELEZIONO ELEMENTI UTILI IN HTML & VARIABILI PREIMPOSTATE
 const bottone = document.getElementById("btn");
 const container = document.getElementById("general-container");
+
 // FUNZIONE CREA ELEMENTO
 function creaElemento(tag, classe){
     const elemento = document.createElement(tag);
     elemento.classList.add(classe);
     return elemento;
-}
+};
+
 // AL CLICK SUL BOTTONE INSERISCO ELEMENTO SQUARE ALLINTERNO DEL CONTAINER
-    // CREO CICLO FOR
-        // CREO ELEMENTO DIV CON LA FUNZIONE E ASSEGNO LA CLASSE SQUARE
-        // INSERICO DENTRO AL DIV IL NUMERO DEL CICLO
+bottone.addEventListener("click", 
+    function(){
+        // CREO CICLO FOR
+        for (let i = 1; i <= 100; i++){
+            // CREO ELEMENTO DIV CON LA FUNZIONE E ASSEGNO LA CLASSE SQUARE
+            const quadrato = creaElemento("div", "square");
+            // INSERICO DENTRO AL DIV IL NUMERO DEL CICLO
+            quadrato.append(i);
+            container.append(quadrato);
+            // AL CLICK SULL'ELEMENTO SQUARE
+            quadrato.addEventListener("click",
+                function(){
+                    // AGGIUNGO ALL'ELEMENTO CLICCATO LA CLASSE
+                    quadrato.classList.add("bg-blu");
+                    // OUTPUT IN CONSOLE DEL NUMERO CLICCATO
+                    console.log("Hai selezionato la cella " + i);  
+                }
+            )   
+        }
+    }
+);
 
-    // AL CLICK SULL'ELEMENTO SQUARE
-        // AGGIUNGO ALL'ELEMENTO CLICCATO LA CLASSE
-        // OUTPUT IN CONSOLE DEL NUMERO CLICCATO
+
+    
+
+    
 
 
 
+        
